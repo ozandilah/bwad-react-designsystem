56af63f8e039baec045965c0048595dbc3c5cb1f
ozandilah <79375569+ozandilah@users.noreply.github.com>
2023-03-26 13:10:23 +0700

    Menambahkan manager storybook

---
+++ .storybook/laracamp.js
@@ -0,0 +1,11 @@
+// .storybook/YourTheme.js
+
+import { create } from "@storybook/theming";
+
+export default create({
+  base: "light",
+  brandTitle: "BWADSREACT",
+  brandUrl: "https://bwad-react-designsystem.netlify.app/",
+  brandImage: "/logo.png",
+  brandTarget: "_self",
+});
